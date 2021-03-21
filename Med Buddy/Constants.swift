ce256f84ac33d64c9a6909ac0289c71eda4ac11b
Grant Sivley <grant.sivley@gmail.com>
2021-03-21 15:37:26 -0700

    init

---
+++ Med Buddy/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  K.swift
+//  Med Buddy
+//
+//  Created by Grant Sivley on 3/21/21.
+//
+
+import UIKit
+
+struct K {
+    
+    struct colors {
+        static let drugBackgroundGrey = "DrugBackgroundGrey"
+        static let drugDarkBlue = "DrugDarkBlue"
+        static let drugGreen = "DrugGreen"
+        static let drugOrange = "DrugOrange"
+        static let drugWhite = "DrugWhite"
+    }
+        
+    
+}
