5a658fba5bccc46ee27b9385e6f81dca68dc27ee
Grant Sivley <grant.sivley@gmail.com>
2021-03-21 18:44:06 -0700

    Make circle button look good on all phones

--- Med Buddy/CustomTabBarViewController.swift
+++ Med Buddy/CustomTabBarViewController.swift
@@ -13,23 +13,45 @@ class CustomTabBarViewController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        tabBar.unselectedItemTintColor = UIColor(named: K.colors.drugBackgroundGrey)
+        tabBar.tintColor = UIColor(named: K.colors.drugDarkBlue)
         
     }
     
     override func viewWillLayoutSubviews() {
+        addCircleButton()
+    }
+    
+    
+    func addCircleButton() {
+     
         let buttonWidth = UIScreen.main.bounds.width / 5
         let buttonX = buttonWidth * 2
-        let buttonY = (tabBar.frame.height / 3) * -1
+        var buttonY = (tabBar.frame.height / 3) * -1
         
+        if (buttonWidth / 4) < (buttonY * -1) {
+            print("This is if")
+        } else {
+            buttonY = (tabBar.frame.height / 1.5) * -1
+        }
+
         let button = UIButton(frame: CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonWidth))
         
         // Currently need to add Plus button image and the button functionality
         
         button.layer.cornerRadius = buttonWidth / 2
         button.backgroundColor = UIColor(named: K.colors.drugOrange)
-        button.setBackgroundImage(UIImage(systemName: "plus"), for: .normal)
+
+        let lightConfiguration = UIImage.SymbolConfiguration(weight: .thin)
+
+        button.setImage(UIImage(systemName: "plus", withConfiguration: lightConfiguration), for: .normal)
         
+        button.contentVerticalAlignment = .fill
+        button.contentHorizontalAlignment = .fill
+        
+        let imageInsets = buttonWidth / 3.5
+        
+        button.imageEdgeInsets = UIEdgeInsets(top: imageInsets, left: imageInsets, bottom: imageInsets, right: imageInsets)
         tabBar.addSubview(button)
     }
     
