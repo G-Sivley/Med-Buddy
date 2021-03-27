841ec08ea2d4cd9a8d69a4263145c5d63a93069a
Grant Sivley <grant.sivley@gmail.com>
2021-03-27 14:54:59 -0700

    Setup cell with shadow and corner radius

--- Med Buddy/Medication Cell/MedicationTableViewCell.swift
+++ Med Buddy/Medication Cell/MedicationTableViewCell.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class MedicationTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var cellView: UIView!
     @IBOutlet weak var medicationNameLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
+        let radius = cellView.frame.height / 4
+        cellView.layer.cornerRadius = radius
+        
+        cellView.layer.masksToBounds = false
+        cellView.layer.shadowColor = UIColor.black.cgColor
+        cellView.layer.shadowOpacity = 0.2
+        cellView.layer.shadowOffset = CGSize(width: -6, height: 4)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
