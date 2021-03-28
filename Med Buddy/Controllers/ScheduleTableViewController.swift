fbf115a56e1f9421e5f33a6e6db1d256beded2e1
Grant Sivley <grant.sivley@gmail.com>
2021-03-27 22:13:53 -0700

    added time label to be even with big label lot of problems though

--- Med Buddy/Controllers/ScheduleTableViewController.swift
+++ Med Buddy/Controllers/ScheduleTableViewController.swift
@@ -11,6 +11,8 @@ class ScheduleTableViewController: UITableViewController {
 
     let medicationsArray = ["Fluoxetine", "Prasugrel", "Ibuprofen"]
     
+    let barLabelView = UILabel()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +31,9 @@ class ScheduleTableViewController: UITableViewController {
        
         
         // Setup NavBar
+        //setupNavBar()
+        setupUI()
         setupNavBar()
-        
         
     }
     
@@ -43,16 +46,31 @@ class ScheduleTableViewController: UITableViewController {
     func setupNavBar() {
         configureNavigationBar(largeTitleColor: UIColor(named: K.colors.drugDarkBlue)!, backgoundColor: UIColor.white, tintColor: UIColor(named: K.colors.drugDarkBlue)!, title: "Schedule", preferredLargeTitle: true)
         
+        self.navigationController?.navigationBar.layer.masksToBounds = false
+        self.navigationController?.navigationBar.layer.shadowColor = UIColor.black.cgColor
+        self.navigationController?.navigationBar.layer.shadowOpacity = 0.2
+        self.navigationController?.navigationBar.layer.shadowOffset = CGSize(width: -6, height: 6)
+        self.navigationController?.navigationBar.layer.shadowRadius = 2
+        
+        
+        
         if let navigationBar = self.navigationController?.navigationBar {
+            
             
             // Add Rounded corners
             
-            addRoundedCorner(OnNavigationBar: navigationBar, cornerRadius: 25)
+            //addRoundedCorner(OnNavigationBar: navigationBar, cornerRadius: 25)
             
             // Time Label
             
-            addTimeLabel(toNavBar: navigationBar, time: "3:22 PM")
+            // addTimeLabel(toNavBar: navigationBar, time: "3:22 PM")
+            
+            
+            
+    
         }
+        
+        
         
     }
     
@@ -130,5 +148,93 @@ class ScheduleTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    private struct Const {
+        /// Image height/width for Large NavBar state
+        static let ImageSizeForLargeState: CGFloat = 40
+        /// Margin from right anchor of safe area to right anchor of Image
+        static let ImageRightMargin: CGFloat = 16
+        /// Margin from bottom anchor of NavBar to bottom anchor of Image for Large NavBar state
+        static let ImageBottomMarginForLargeState: CGFloat = 12
+        /// Margin from bottom anchor of NavBar to bottom anchor of Image for Small NavBar state
+        static let ImageBottomMarginForSmallState: CGFloat = 12
+        /// Image height/width for Small NavBar state
+        static let ImageSizeForSmallState: CGFloat = 22
+        /// Height of NavBar for Small state. Usually it's just 44
+        static let NavBarHeightSmallState: CGFloat = 44
+        /// Height of NavBar for Large state. Usually it's just 96.5 but if you have a custom font for the title, please make sure to edit this value since it changes the height for Large state of NavBar
+        static let NavBarHeightLargeState: CGFloat = 96.5
+        
+        static let NavBarFontLargeState: CGFloat = 36
+        static let NavBarFontSmallState: CGFloat = 16
+        
+    }
+    private func setupUI() {
+      navigationController?.navigationBar.prefersLargeTitles = true
+      title = "Schedule"
+        
+        
+        barLabelView.frame = CGRect(x: 0, y: 0, width: 130, height: 43)
+        
+        barLabelView.backgroundColor = .clear
+
+        barLabelView.textColor = UIColor(named: K.colors.drugDarkBlue)
+
+        barLabelView.font = UIFont(name: "Quicksand-Regular", size: Const.NavBarFontLargeState)
+        barLabelView.attributedText = NSMutableAttributedString(string: "3:33 PM", attributes: [NSAttributedString.Key.kern: -1.8])
+        // Initial setup for image for Large NavBar state since the the screen always has Large NavBar once it gets opened
+        guard let navigationBar = self.navigationController?.navigationBar else { return }
+        
+        navigationBar.addSubview(barLabelView)
+        
+        // setup constraints
+        
+        barLabelView.clipsToBounds = true
+        barLabelView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            barLabelView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor, constant: -Const.ImageRightMargin),
+            barLabelView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: -Const.ImageBottomMarginForLargeState),
+            barLabelView.heightAnchor.constraint(equalToConstant: Const.ImageSizeForLargeState),
+            
+            //view.widthAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+    }
+    private func moveAndResizeImage(for height: CGFloat) {
+        let coeff: CGFloat = {
+            let delta = height - Const.NavBarHeightSmallState
+            let heightDifferenceBetweenStates = (Const.NavBarHeightLargeState - Const.NavBarHeightSmallState)
+            return delta / heightDifferenceBetweenStates
+        }()
+
+        let factor = Const.ImageSizeForSmallState / Const.ImageSizeForLargeState
+
+        let scale: CGFloat = {
+            let sizeAddendumFactor = coeff * (1.0 - factor)
+            return min(1.0, sizeAddendumFactor + factor)
+        }()
+
+        // Value of difference between icons for large and small states
+        let sizeDiff = Const.ImageSizeForLargeState * (1.0 - factor) // 8.0
+        let yTranslation: CGFloat = {
+            /// This value = 14. It equals to difference of 12 and 6 (bottom margin for large and small states). Also it adds 8.0 (size difference when the image gets smaller size)
+            let maxYTranslation = Const.ImageBottomMarginForLargeState - Const.ImageBottomMarginForSmallState + sizeDiff
+            return max(0, min(maxYTranslation, (maxYTranslation - coeff * (Const.ImageBottomMarginForSmallState + sizeDiff))))
+        }()
+
+        let xTranslation = max(0, sizeDiff - coeff * sizeDiff)
+
+        barLabelView.transform = CGAffineTransform.identity
+            .scaledBy(x: scale, y: scale)
+            .translatedBy(x: xTranslation, y: yTranslation)
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let height = navigationController?.navigationBar.frame.height else { return }
+        moveAndResizeImage(for: height)
+        
+  
+    }
+    
+ 
 
 }
