ee552069a7c9f73c67a724edb0b76a08fe37edd5
Grant Sivley <grant.sivley@gmail.com>
2021-04-30 10:47:11 -0700

    Got the async function to work properly while loading the images.

--- Med Buddy/Controllers/ScheduleTableViewController.swift
+++ Med Buddy/Controllers/ScheduleTableViewController.swift
@@ -116,8 +116,34 @@ class ScheduleTableViewController: UITableViewController  {
             cell.medicationNameLabel.text = medication.name
             
             
-            cell.medicationImg.image = medication.img
+            // This works as expected, but has major code smell. I will refactor to ensure that we are not loading the images multiple times during table initializations.
+            let storageRef = Storage.storage().reference(withPath: "medications/F1711963-E131-4102-AEFB-992057CAA484.jpg")
+            storageRef.getData(maxSize: 4 * 1024 * 1024) { (data, error) in
+                if let e = error {
+                    print("Got an error fetching data: \(e.localizedDescription)")
+                    return
+                } else {
+                    if let data = data {
+                        DispatchQueue.main.async {
+                            if let imgFromData = UIImage(data: data) {
+                                cell.medicationImg.image = imgFromData
+                                print("Image Associated")
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            
+            
+            
+            
+            
             print("med image loaded")
+            
+            
+            
+            
             
             
             
@@ -131,7 +157,7 @@ class ScheduleTableViewController: UITableViewController  {
         deleteMedication(indexPath: indexPath)
         
     }
-
+    
     //MARK: - Nib Registration
     
     func registerNibs() {
@@ -148,79 +174,57 @@ class ScheduleTableViewController: UITableViewController  {
         self.tableView.register(addMedCellNib, forCellReuseIdentifier: K.Cells.addMedCell)
         
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     //MARK: - Data Manipulation
     
     func loadMedications() {
-        var img = UIImage()
         
-        
-        // Image is not being populated before getting to the medications array
-        let storageRef = Storage.storage().reference(withPath: "medications/F1711963-E131-4102-AEFB-992057CAA484.jpg")
-        storageRef.getData(maxSize: 4 * 1024 * 1024) { (data, error) in
-            if let e = error {
-                print("Got an error fetching data: \(e.localizedDescription)")
-                return
-            } else {
-                if let data = data {
-                    print(data)
-                    if let imgFromData = UIImage(data: data) {
-                        img = imgFromData
-                      
-    
-                    }
-                }
-            }
-        }
-
-        
-        
-        db.collection("medications").order(by: "name").addSnapshotListener { (querySnapshot, error) in
+        self.db.collection("medications").order(by: "name").addSnapshotListener { (querySnapshot, error) in
             self.medicationsArray = []
             
             if let e = error {
@@ -233,7 +237,7 @@ class ScheduleTableViewController: UITableViewController  {
                             
                             // Need to fix this later. This could be dangerous
                             
-                            let medication = Medication(name: medName, id: doc.documentID, img: img)
+                            let medication = Medication(name: medName, id: doc.documentID)
                             self.medicationsArray.append(medication)
                             
                             DispatchQueue.main.async {
@@ -244,7 +248,11 @@ class ScheduleTableViewController: UITableViewController  {
                 }
             }
         }
+        
     }
+    
+    
+    
     
     // Function that deletes a medication based on its index path
     func deleteMedication(indexPath: IndexPath) {
